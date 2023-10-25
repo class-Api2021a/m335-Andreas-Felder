a35e3e1554527e4c9a039ec29d64b58dde3fd4c1
Noah Bargisen <bargisenn@bzz.ch>
2023-10-25 09:39:11 +0200

    renaming

--- app/src/main/java/com/ubs/mycurrency/MainActivity.java
+++ app/src/main/java/com/ubs/mycurrency/MainActivity.java
@@ -3,6 +3,9 @@ package com.ubs.mycurrency;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void loadFlag(String flagDefinition) {
+        // Find the ImageView in your layout
+        ImageView imageView = findViewById(R.id.mainCountryImage);
+
+        // Use Picasso to load the image into the ImageView
+        Picasso.get().load(flagDefinition).into(imageView);
     }
 }
