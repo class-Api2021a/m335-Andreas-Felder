3fbb896088b00d352d7e1107523715962c1a0acf
Noah Bargisen <bargisenn@bzz.ch>
2023-10-30 08:52:33 +0100

    keyboard working

--- app/src/main/java/com/ubs/mycurrency/MainActivity.java
+++ app/src/main/java/com/ubs/mycurrency/MainActivity.java
@@ -3,8 +3,12 @@ package com.ubs.mycurrency;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +16,8 @@ import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 import com.ubs.mycurrency.api.ExchangeRateAPI;
 import com.ubs.mycurrency.record.ExchangeRate;
+import com.ubs.mycurrency.service.CalculatorButtonClickListener;
+import com.ubs.mycurrency.service.NoImeEditText;
 import com.ubs.mycurrency.util.Currency;
 
 import java.util.ArrayList;
@@ -26,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
     private final Map<Integer, Currency> currencyMap = new HashMap<>();
 
+    private final List<Integer> buttonIds = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //prevents that dark mode makes the UI unusable by stopping it from making all white elements dark
@@ -33,6 +41,52 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridLayout gridLayout = findViewById(R.id.gridLayout);
+
+        // Loop through the GridLayout to get the Button IDs
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            View child = gridLayout.getChildAt(i);
+            if (child instanceof Button) {
+                Button button = (Button) child;
+                buttonIds.add(button.getId());
+            }
+        }
+
+        NoImeEditText mainEditText = findViewById(R.id.mainCurrencyInput);
+        NoImeEditText secondaryEditText = findViewById(R.id.secondaryCurrencyInput);
+
+        CalculatorButtonClickListener mainButtonClickListener = new CalculatorButtonClickListener(this, mainEditText);
+        CalculatorButtonClickListener secondaryButtonClickListener = new CalculatorButtonClickListener(this, secondaryEditText);
+
+        View.OnFocusChangeListener focusChangeListener = new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    // Perform actions for the selected EditText
+                    if (v == mainEditText) {
+                        for (int buttonId : buttonIds) {
+                            Button button = findViewById(buttonId);
+                            button.setOnClickListener(mainButtonClickListener);
+                        }
+                    } else if (v == secondaryEditText) {
+                        for (int buttonId : buttonIds) {
+                            Button button = findViewById(buttonId);
+                            button.setOnClickListener(secondaryButtonClickListener);
+                        }
+                    }
+                }
+            }
+        };
+
+        mainEditText.setOnFocusChangeListener(focusChangeListener);
+        secondaryEditText.setOnFocusChangeListener(focusChangeListener);
+
+
+
+
+
+
         currencyMap.put(1, Currency.getEnumFromString(Currency.class, "CHF")); //Default
         currencyMap.put(2, Currency.getEnumFromString(Currency.class,"EUR"));  //Default
         loadMainFlag(getFlagDefinition(Objects.requireNonNull(currencyMap.get(1)).getIsoCountryCode()));
--- app/src/main/java/com/ubs/mycurrency/service/KeyboardService.java
+++
@@ -1,4 +0,0 @@
-package com.ubs.mycurrency.service;
-
-public class KeyboardService {
-}
