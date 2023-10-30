00756451e52a0148894eadfdfab1b5c9e55c1f88
AndyI0I <andreasffelder@hotmail.com>
2023-10-30 09:20:10 +0100

    stuff

--- app/src/main/java/com/ubs/mycurrency/MainActivity.java
+++ app/src/main/java/com/ubs/mycurrency/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
